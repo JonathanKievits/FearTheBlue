2a1a3fc8451f62686c14844ad1cab5b171e16b00
JonathanKievits <Jonathan.kievits@hotmail.nl>
2017-04-07 09:08:20 +0200

    Controller input added

---
+++ FearTheBlue/Assets/_Scripts/ControllerMovingScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovingScript : MonoBehaviour {
+    
+    private float x;
+    private float z;
+
+
+    void Update () {
+
+        if (Input.GetJoystickNames().Length > 0)
+        {
+            x = Input.GetAxisRaw("LeftStickX");
+            z = Input.GetAxisRaw("LeftStickY");
+        }
+        else
+        {
+            x = Input.GetAxisRaw("Horizontal");
+            z = Input.GetAxisRaw("Vertical");
+        }
+
+        if (Input.GetButtonDown("Square"))
+	    {
+	        Debug.Log("You pressed Square");
+	    }
+        if (Input.GetButtonDown("X"))
+        {
+            Debug.Log("You pressed X");
+        }
+        if (Input.GetButtonDown("Circle"))
+        {
+            Debug.Log("You pressed Circle");
+        }
+        if (Input.GetButtonDown("Triangle"))
+        {
+            Debug.Log("You pressed Triangle");
+        }
+    }
+}
