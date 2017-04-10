733fad66b890e2738fd167293c755a6297be9a31
JonathanKievits <Jonathan.kievits@hotmail.nl>
2017-04-10 15:12:11 +0200

    Added controller input

--- FearTheBlue/Assets/_Scripts/ControllerMovingScript.cs
+++ FearTheBlue/Assets/_Scripts/ControllerMovingScript.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ControllerMovingScript : MonoBehaviour {
-    
+public class ControllerMovingScript : MonoBehaviour
+{
+
+    private Vector3 movement;
     private float x;
     private float z;
+    private float speed;
+    private Rigidbody rigidBody;
+    [HideInInspector]public Transform playerPos;
 
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
 
-    void Update () {
+    void Start()
+    {
+        speed = 10;
+    }
+
+    void Update ()
+    {
 
         if (Input.GetJoystickNames().Length > 0)
         {
@@ -37,5 +52,12 @@ public class ControllerMovingScript : MonoBehaviour {
         {
             Debug.Log("You pressed Triangle");
         }
+        movement = transform.forward.normalized * z;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 velocity = movement * speed * Time.fixedDeltaTime;
+        rigidBody.MovePosition(rigidBody.position + velocity);
     }
 }
