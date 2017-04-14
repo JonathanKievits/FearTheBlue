700780a4be7f49c8a4e9fe3b06a98bfe9fc9c71c
JonathanKievits <Jonathan.kievits@hotmail.nl>
2017-04-14 11:55:58 +0200

    Added rotation

---
+++ FearTheBlue/Assets/_Scripts/Player/CameraRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour {
+
+    [Range(1, 5)][SerializeField] private float turnSpeed;
+
+    void Update()
+    {
+        var x = Input.GetAxis(Controller.RightStickX);
+
+        transform.Rotate(0,x * turnSpeed,0);
+    }
+}
