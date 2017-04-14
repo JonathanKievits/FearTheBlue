63a41988b883124b6dce55b953874f60f24faad7
Danivdwerf <danivdwerf.com>
2017-04-14 09:33:55 +0200

    rotate object with the controller

---
+++ FearTheBlue/Assets/RotateObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RotateObject : MonoBehaviour 
+{
+	private float angleX, angleY;
+	private void Start()
+	{
+		angleX = 0;
+		angleY = 0;
+	}
+	private void Update()
+	{
+		angleX += Input.GetAxisRaw (Controller.RightStickX);
+		angleY += Input.GetAxisRaw (Controller.RightStickY);
+		var direction = new Vector3 (this.transform.localEulerAngles.x, angleX, angleY);
+
+		this.transform.localEulerAngles = direction;
+		/*
+		if (Input.GetButton (Controller.R1))
+		{
+			angle++;
+			this.transform.localEulerAngles = new Vector3 (this.transform.eulerAngles.x, angle, this.transform.eulerAngles.z);
+		}
+
+		if (Input.GetButton (Controller.L1))
+		{
+			angle--;
+			this.transform.localEulerAngles = new Vector3 (this.transform.eulerAngles.x, angle, this.transform.eulerAngles.z);
+		}
+		*/
+	}
+}
