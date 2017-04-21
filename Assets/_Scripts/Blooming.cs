b5ff0fd698e026ecd1b578d81edd3cc9d1fc806d
Danivdwerf <danivdwerf.com>
2017-04-21 09:35:48 +0200

    update

---
+++ Assets/_Scripts/Blooming.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[AddComponentMenu("Image Effects/Aubergine/BloomSimple")]
+public class Blooming : MonoBehaviour 
+{
+	[SerializeField]private float strength = 0.5f;
+	[SerializeField]private Shader shader;
+	private Material m_Material;
+	protected Material material 
+	{
+		get 
+		{
+			if (m_Material == null) 
+			{
+				m_Material = new Material(shader);
+				m_Material.hideFlags = HideFlags.HideAndDontSave;
+			}
+			return m_Material;
+		}
+	}
+
+	protected void Start () 
+	{
+		if (!SystemInfo.supportsImageEffects) 
+		{
+			enabled = false;
+			return;
+		}
+		if (!shader || !shader.isSupported)
+			enabled = false;
+	}
+	protected void OnDisable () 
+	{
+		if( m_Material )
+			DestroyImmediate(m_Material);
+	}
+
+	void Awake () 
+	{
+		material.SetFloat("_Strength", strength);
+	}
+		
+	void OnRenderImage (RenderTexture source, RenderTexture destination) 
+	{
+		material.SetFloat("_Strength", strength);
+		Graphics.Blit (source, destination, material);
+	}
+}
---
+++ Assets/_Scripts/RenderScale.cs.orig
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+namespace VRStandardAssets.Examples
+{
+    public class RenderScale : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_RenderScale = 1.5f;
+
+        void Start()
+        {
+            VRSettings.renderScale = m_RenderScale;
+        }
+    }
+}
